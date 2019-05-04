12be5065a0f1d4c952165eec868a6de8d53281f0
Karen Javadyan <karen@teamable.com>
2019-05-05 02:11:19 +0400

    add celery add ability to send telegram messages via celery

---
+++ tasks.py
@@ -0,0 +1,31 @@
+import telegram
+from celery import Celery
+from celery.schedules import crontab
+
+app = Celery('tasks', broker="amqp://guest:guest@rabbitmq:5672")
+
+
+@app.task
+def send_telegram_message(chat_id):
+    bot = telegram.Bot(token="665864512:AAHJ7mnoPo7KhrJx7XKwUoEFRgmjQXRWkYo")
+    bot.send_message(chat_id=chat_id, text="Test message")
+
+
+@app.task
+def send_posts_to_users():
+    # TODO get users from database
+    users = [1, 2]
+
+    for _ in users:
+        # TODO use celery chunks here
+        send_telegram_message.delay(59911481)
+
+    return "URA"
+
+
+app.conf.beat_schedule = {
+    "send_posts_to_users": {
+        "task": "tasks.send_posts_to_users",
+        "schedule": crontab()
+    }
+}
